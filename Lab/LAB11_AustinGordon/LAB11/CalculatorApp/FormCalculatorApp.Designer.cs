333ad01ec09ded57589a4588da76caa5699c753d
Austin Gordon <agordon4@msunet2k.edu>
2018-04-26 10:34:39 -0500

    Completed Lab 11

---
+++ Lab/LAB11_AustinGordon/LAB11/CalculatorApp/FormCalculatorApp.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace CalculatorApp
+{
+    partial class CalculatorAppForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxValue1 = new System.Windows.Forms.TextBox();
+            this.textBoxValue2 = new System.Windows.Forms.TextBox();
+            this.buttonMultiply = new System.Windows.Forms.Button();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxDate = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 79);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Simple Calculator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "First Value:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Second Value:";
+            // 
+            // textBoxValue1
+            // 
+            this.textBoxValue1.Location = new System.Drawing.Point(200, 143);
+            this.textBoxValue1.Name = "textBoxValue1";
+            this.textBoxValue1.Size = new System.Drawing.Size(100, 26);
+            this.textBoxValue1.TabIndex = 3;
+            this.textBoxValue1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxValue2
+            // 
+            this.textBoxValue2.Location = new System.Drawing.Point(200, 209);
+            this.textBoxValue2.Name = "textBoxValue2";
+            this.textBoxValue2.Size = new System.Drawing.Size(100, 26);
+            this.textBoxValue2.TabIndex = 4;
+            this.textBoxValue2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // buttonMultiply
+            // 
+            this.buttonMultiply.Location = new System.Drawing.Point(54, 276);
+            this.buttonMultiply.Name = "buttonMultiply";
+            this.buttonMultiply.Size = new System.Drawing.Size(80, 36);
+            this.buttonMultiply.TabIndex = 5;
+            this.buttonMultiply.Text = "Multiply";
+            this.buttonMultiply.UseVisualStyleBackColor = true;
+            this.buttonMultiply.Click += new System.EventHandler(this.buttonMultiply_Click);
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(151, 276);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(80, 36);
+            this.buttonAdd.TabIndex = 6;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(54, 337);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(80, 36);
+            this.buttonReset.TabIndex = 8;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(50, 409);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Result:";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult.Location = new System.Drawing.Point(119, 410);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(225, 20);
+            this.labelResult.TabIndex = 10;
+            this.labelResult.Text = "Messages will display here.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(264, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Date:";
+            // 
+            // textBoxDate
+            // 
+            this.textBoxDate.Location = new System.Drawing.Point(330, 19);
+            this.textBoxDate.Name = "textBoxDate";
+            this.textBoxDate.Size = new System.Drawing.Size(100, 26);
+            this.textBoxDate.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(248, 276);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 36);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Divide";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // CalculatorAppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(476, 488);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxDate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.buttonMultiply);
+            this.Controls.Add(this.textBoxValue2);
+            this.Controls.Add(this.textBoxValue1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "CalculatorAppForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator App";
+            this.Load += new System.EventHandler(this.CalculatorAppForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxValue1;
+        private System.Windows.Forms.TextBox textBoxValue2;
+        private System.Windows.Forms.Button buttonMultiply;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxDate;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lab/LAB11_AustinGordon/LAB11/CalculatorApp/FormCalculatorApp.cs
@@ -0,0 +1,143 @@
+﻿/* CalculatorApp.cs
+ * This application allows the user
+ * to input two numeric values. Buttons
+ * for Add and Multiply are available which
+ * display the results of the calculations.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApp
+{
+    public partial class CalculatorAppForm : Form
+    {
+        public CalculatorAppForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculatorAppForm_Load(object sender, EventArgs e)
+        {
+            textBoxDate.Text = DateTime.Now.ToString("d"); // display today's date in mm/dd/yyyy fashion
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            textBoxValue1.Text = "";
+            textBoxValue2.Text = "";
+            textBoxValue1.Focus();
+            labelResult.ForeColor = Color.Black;
+            labelResult.Text = "";
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            double val1, val2;
+
+            // validate first input box
+            if(double.TryParse(textBoxValue1.Text, out val1) == false || val1 < 0)
+            {
+                labelResult.ForeColor = Color.Red;
+                labelResult.Text = "Value must be numeric and positive.";
+                textBoxValue1.Focus();
+                return;
+            }
+
+            // validate second input box
+            if (double.TryParse(textBoxValue2.Text, out val2) == false || val2 < 0)
+            {
+                labelResult.ForeColor = Color.Red;
+                labelResult.Text = "Value must be numeric and positive.";
+                textBoxValue2.Focus();
+                return;
+            }
+
+            // when control reaches here, it means both values are valid
+            labelResult.Text = ""; // clear the result box
+
+            // do the calculation and display the result
+            double result = val1 + val2;
+            labelResult.ForeColor = Color.DarkBlue;
+            labelResult.Text = result.ToString();
+
+        }
+
+        private void buttonMultiply_Click(object sender, EventArgs e)
+        {
+            double val1, val2;
+
+            // validate first input box
+            if (double.TryParse(textBoxValue1.Text, out val1) == false || val1 < 0)
+            {
+                labelResult.ForeColor = Color.Red;
+                labelResult.Text = "Value must be numeric and positive.";
+                textBoxValue1.Focus();
+                return;
+            }
+
+            // validate second input box
+            if (double.TryParse(textBoxValue2.Text, out val2) == false || val2 < 0)
+            {
+                labelResult.ForeColor = Color.Red;
+                labelResult.Text = "Value must be numeric and positive.";
+                textBoxValue2.Focus();
+                return;
+            }
+
+            // when control reaches here, it means both values are valid
+            labelResult.Text = ""; // clear the result box
+
+            // do the calculation and display the result
+            double result = val1 * val2;
+            labelResult.ForeColor = Color.DarkBlue;
+            labelResult.Text = result.ToString();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double val1, val2;
+
+            // validate first input box
+            if (double.TryParse(textBoxValue1.Text, out val1) == false || val1 < 0)
+            {
+                labelResult.ForeColor = Color.Red;
+                labelResult.Text = "Value must be numeric and positive.";
+                textBoxValue1.Focus();
+                return;
+            }
+
+            // validate second input box
+            if (double.TryParse(textBoxValue2.Text, out val2) == false || val2 < 0)
+            {
+                labelResult.ForeColor = Color.Red;
+                labelResult.Text = "Value must be numeric and positive.";
+                textBoxValue2.Focus();
+                return;
+
+            }else if(val2 == 0)
+            {
+                labelResult.ForeColor = Color.Red;
+                labelResult.Text = "Denominator cannot be 0.";
+                textBoxValue2.Focus();
+                return;
+            }
+
+            // when control reaches here, it means both values are valid
+            labelResult.Text = ""; // clear the result box
+
+            // do the calculation and display the result
+            double result = Math.Round(val1 / val2, 2);
+            labelResult.ForeColor = Color.DarkBlue;
+            labelResult.Text = result.ToString();
+
+        }
+    } //class
+} //namespace
